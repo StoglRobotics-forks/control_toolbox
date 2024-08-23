01218f1889bfa36dca76c7c067308fe5315bd453
Daniel Azanov <daniel.azanov@stoglrobotics.de>
2024-08-23 09:18:23 +0200

    add transform filter

---
+++ include/control_filters/transform_filter.hpp
@@ -0,0 +1,148 @@
+// Copyright (c) 2023, Stogl Robotics Consulting UG (haftungsbeschränkt)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CONTROL_FILTERS__TRANSFORM_FILTER_HPP_
+#define CONTROL_FILTERS__TRANSFORM_FILTER_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "filters/filter_base.hpp"
+#include "geometry_msgs/msg/wrench_stamped.hpp"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+#include "transform_filter_parameters.hpp"
+
+namespace control_filters
+{
+
+template <typename T>
+class TransformFilter : public filters::FilterBase<T>
+{
+public:
+  /** \brief Constructor */
+  TransformFilter();
+
+  /** \brief Destructor */
+  ~TransformFilter();
+
+  /** @brief Configure filter parameters  */
+  bool configure() override;
+
+  /** \brief Update the filter and return the data separately
+   * \param data_in T array with length width
+   * \param data_out T array with length width
+   */
+  bool update(const T & data_in, T & data_out) override;
+
+protected:
+  bool compute_internal_params()
+  {
+    if (parameters_.target_frame_id.empty())
+    {
+      RCLCPP_ERROR((*logger_), "Target frame id is empty");
+      return false;
+    }
+
+    if (parameters_.sensor_frame_id.empty())
+    {
+      RCLCPP_ERROR((*logger_), "Sensor frame id is empty");
+      return false;
+    }
+
+    target_frame_id_ = parameters_.target_frame_id;
+    sensor_frame_id_ = parameters_.sensor_frame_id;
+    return true;
+  };
+
+private:
+  rclcpp::Clock::SharedPtr clock_;
+  std::shared_ptr<rclcpp::Logger> logger_;
+  std::shared_ptr<transform_filter::ParamListener> parameter_handler_;
+  transform_filter::Params parameters_;
+
+  // Frames for Transformation of Gravity / CoG Vector
+  // std::string world_frame_;   // frame in which gravity is given
+  // std::string sensor_frame_;  // frame in which Cog is given and compution occur
+  // Storage for Calibration Values
+  // geometry_msgs::msg::Vector3Stamped cog_;            // Center of Gravity Vector (wrt sensor
+  // frame) geometry_msgs::msg::Vector3Stamped cst_ext_force_;  // Gravity Force Vector (wrt world
+  // frame)
+  // geometry_msgs::msg::WrenchStamped wrench_threshold_;
+  std::string sensor_frame_id_;
+  std::string target_frame_id_;
+
+  // Filter objects
+  std::unique_ptr<tf2_ros::Buffer> p_tf_Buffer_;
+  std::unique_ptr<tf2_ros::TransformListener> p_tf_Listener_;
+  geometry_msgs::msg::TransformStamped transform_;
+  // geometry_msgs::msg::TransformStamped transform_sensor_datain_, transform_world_dataout_,
+  //   transform_data_out_sensor_, transform_sensor_world_;
+};
+
+template <typename T>
+TransformFilter<T>::TransformFilter()
+{
+}
+
+template <typename T>
+TransformFilter<T>::~TransformFilter()
+{
+}
+
+template <typename T>
+bool TransformFilter<T>::configure()
+{
+  clock_ = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
+  p_tf_Buffer_.reset(new tf2_ros::Buffer(clock_));
+  p_tf_Listener_.reset(new tf2_ros::TransformListener(*p_tf_Buffer_.get(), true));
+
+  logger_.reset(
+    new rclcpp::Logger(this->logging_interface_->get_logger().get_child(this->filter_name_)));
+
+  // Initialize the parameter_listener once
+  if (!parameter_handler_)
+  {
+    try
+    {
+      parameter_handler_ = std::make_shared<transform_filter::ParamListener>(
+        this->params_interface_, this->param_prefix_);
+    }
+    catch (rclcpp::exceptions::ParameterUninitializedException & ex)
+    {
+      RCLCPP_ERROR((*logger_), "Threshold filter cannot be configured: %s", ex.what());
+      parameter_handler_.reset();
+      return false;
+    }
+    catch (rclcpp::exceptions::InvalidParameterValueException & ex)
+    {
+      RCLCPP_ERROR((*logger_), "Threshold filter cannot be configured: %s", ex.what());
+      parameter_handler_.reset();
+      return false;
+    }
+  }
+
+  parameters_ = parameter_handler_->get_params();
+  if (!compute_internal_params())
+  {
+    return false;
+  }
+
+  return true;
+}
+
+}  // namespace control_filters
+
+#endif  // CONTROL_FILTERS__TRANSFORM_FILTER_HPP_
---
+++ src/control_filters/transform_filter.cpp
@@ -0,0 +1,69 @@
+// Copyright (c) 2023, Stogl Robotics Consulting UG (haftungsbeschränkt)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "control_filters/transform_filter.hpp"
+
+// #include "geometry_msgs/msg/vector3_stamped.hpp"
+// #include "geometry_msgs/msg/wrench_stamped.hpp"
+// #include "tf2/LinearMath/Vector3.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
+namespace control_filters
+{
+template <>
+bool TransformFilter<geometry_msgs::msg::WrenchStamped>::update(
+  const geometry_msgs::msg::WrenchStamped & data_in, geometry_msgs::msg::WrenchStamped & data_out)
+{
+  if (!this->configured_)
+  {
+    RCLCPP_ERROR_SKIPFIRST_THROTTLE((*logger_), *clock_, 2000, "Filter is not configured");
+    return false;
+  }
+
+  // Update internal parameters if required
+  if (parameter_handler_->is_old(parameters_))
+  {
+    parameters_ = parameter_handler_->get_params();
+    if (!compute_internal_params())
+    {
+      RCLCPP_ERROR_THROTTLE((*logger_), *clock_, 2000, "Failed to compute internal parameters");
+      return false;
+    }
+  }
+
+  try
+  {
+    transform_ = p_tf_Buffer_->lookupTransform(target_frame_id_, sensor_frame_id_, rclcpp::Time());
+  }
+  catch (const tf2::TransformException & ex)
+  {
+    RCLCPP_ERROR_SKIPFIRST_THROTTLE(
+      (*logger_), *clock_, 5000, "Could not transform data from '%s' to '%s': %s",
+      data_in.header.frame_id.c_str(), target_frame_id_.c_str(), ex.what());
+    return false;  // if cannot transform, result of subsequent computations is invalid
+  }
+
+  // Transform data_in to target_frame_id
+  tf2::doTransform(data_in, data_out, transform_);
+
+  return true;
+}
+
+}  // namespace control_filters
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  control_filters::TransformFilter<geometry_msgs::msg::WrenchStamped>,
+  filters::FilterBase<geometry_msgs::msg::WrenchStamped>)
